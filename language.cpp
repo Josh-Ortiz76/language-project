6c13de19a53529d08339f69ab7e722874f6dbe85
Tim Zhou <gmtimothy1213@gmail.com>
2017-12-12 17:57:15 -0500

    Fixed cosine similarity
    Fixed the problem in cosine similarity causing tests to be inaccurate

--- language.cpp
+++ language.cpp
@@ -21,7 +21,7 @@ language::language(std::string str){
     infile.close();
   }else{
     exit(EXIT_FAILURE);
-  }
+}
 }
 
 //Returns the occurences of the trigrams found in the text
@@ -37,41 +37,36 @@ std::vector<int> language::trigramHash(){
   //Uses the base 27 to find the hash number of the trigram to compare for other hashs of the same
   for (int z = 0; z < 3; z++){
     for (int i = z; i < (int)text.length(); i += 3){
-      trigram = text.substr(i, 3);
       //Makes sure the trigram does not go out of range of the text
       if (i < (int)text.length() - 1 && i < (int)text.length() - 2){
+        trigram = text.substr(i, 3);
         hash1 = alpha.find(trigram[0]);
         hash2 = alpha.find(trigram[1]);
         hash3 = alpha.find(trigram[2]);
         triHash =  (hash1 * pow(27, 2)) + (hash2 * 27) + (hash3);
         hash.push_back(triHash);
+        freq.at(triHash) += 1;
+        //std::cout << trigram << " " << freq.at(triHash) << std::endl;
       }
     }
-  }
-  int count = 0;
-  //Replaces the position of the trigram with the frequency number
-  for (int i = 0; i < (int)hash.size(); i++){
-    for (int j = 0; j < (int)hash.size(); j++){
-      if (hash[i] == hash[j]){
-        count += 1;
-      }
-    }
-    freq[hash[i]] = count;
-    count = 0;
   }
   return freq;
 }
 
-//
 double language::similarity(std::vector<int> A, std::vector<int> B){
-  double numSum = 0;
-  double sumA = 0;
-  double sumB = 0;
-  for (int i = 0; i < (int)A.size() - 1; i++){
+  unsigned long long numSum = 0;
+  unsigned long long sumA = 0;
+  unsigned long long sumB = 0;
+  double cosSim = 0;
+  for (int i = 0; i < (int)A.size() - 1 && (int)B.size() - 1; i++){
     numSum += A[i] * B[i];
-    sumA += pow(A[i], 2);
-    sumB += pow(B[i], 2);
-    std::cout << i << std::endl;
   }
-  return(numSum/(sqrt(sumA) * sqrt(sumB)));
+  for (int j = 0; j < (int)A.size() - 1; j++){
+    sumA += pow(A[j], 2);
+  }
+  for (int k = 0; k < (int)B.size() - 1; k++){
+    sumB += pow(B[k], 2);
+  }
+  cosSim = numSum/(sqrt(sumA) * sqrt(sumB));
+  return cosSim;
 }
--- main.cpp
+++ main.cpp
@@ -5,22 +5,25 @@
 #include <vector>
 #include "language.h"
 
-int main(int argc, char *argv[]){
-  //language lang = language(argv[1]);
-  //std::vector<int> num = lang.trigramHash();
-
+int main(int argc, char* argv[]){
+  language lang = language(argv[argc - 1]);
+  std::vector<int> vtest = lang.trigramHash();
   //Prints out the frequencies of the trigrams
 
-  std::string closest = "";
+  std::string closest;
   double largest = 0;
-  language test = language(argv[argc - 1]);
-  std::vector<int> vtest = test.trigramHash();
+
+  //language test = language(argv[argc - 1]);
+  //std::vector<int> vtest = test.trigramHash();
+
+  /*for(int z = 0; z < (int)vtest.size(); z++){
+    std::cout << vtest[z] << std::endl;
+  }*/
 
   for (int j = 1; j < argc - 1; j++){
     language train = language(argv[j]);
     std::vector<int> num = train.trigramHash();
     double temp = train.similarity(num, vtest);
-    std::cout << j << std::endl;
     if (temp > largest){
       largest = temp;
       closest = argv[j];
